71bedc8aefaac805dde099e5c6ee78e970ea1fac
Saeed Rasooli <saeed.gnu@gmail.com>
2024-09-11 22:03:40 +0330

    build-from-source/1-clone-sources.sh: change backend url

--- build-from-source/1-clone-sources.sh
+++ build-from-source/1-clone-sources.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -x
 
-[ -d glitchtip-backend ] || git clone https://gitlab.com/glitchtip/glitchtip-backend.git
+[ -d glitchtip-backend ] || git clone https://gitlab.com/ilius/glitchtip-backend.git
 [ -d glitchtip-frontend ] || git clone https://gitlab.com/glitchtip/glitchtip-frontend.git
